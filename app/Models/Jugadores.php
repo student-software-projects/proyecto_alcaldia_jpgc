070ff544706a5f02a48653a9f38ee87047fa7c7e
juangarzon616 <jpgarzon947@misena.edu.co>
2020-11-28 15:25:23 -0500

    models and migrations created

--- app/Models/Jugadores.php
+++ app/Models/Jugadores.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Jugadores extends Model
 {
     use HasFactory;
 }

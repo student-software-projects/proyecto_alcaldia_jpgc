a858d661ca0933a6c078b7c1579f86a6ddcd4b94
juangarzon616 <jpgarzon947@misena.edu.co>
2020-11-28 21:28:42 -0500

    fghdfgh

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -5,22 +5,13 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Dashboard | Skote - Responsive Bootstrap 4 Admin Dashboard</title>
+    <title>Deportes Localidades</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
-
-    <!-- Bootstrap Css -->
-    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="{{ url('css/app.css') }}" rel="stylesheet" type="text/css" />
 
 </head>
-
 
 <body data-sidebar="dark">
 
@@ -1475,20 +1466,7 @@
 <!-- Right bar overlay-->
 <div class="rightbar-overlay"></div>
 
-<!-- JAVASCRIPT -->
-<script src="assets/libs/jquery/jquery.min.js"></script>
-<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/libs/metismenu/metisMenu.min.js"></script>
-<script src="assets/libs/simplebar/simplebar.min.js"></script>
-<script src="assets/libs/node-waves/waves.min.js"></script>
-
-<!-- apexcharts -->
-<script src="assets/libs/apexcharts/apexcharts.min.js"></script>
-
-<script src="assets/js/pages/dashboard.init.js"></script>
-
-<!-- App js -->
-<script src="assets/js/app.js"></script>
+<script src="{{ url('js/app.js') }}"></script>
 </body>
 
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,24 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+/*
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ]);*/
+
+mix.styles([
+    'resources/skote/css/bootstrap.min.css',
+    'resources/skote/css/app.min.css',
+    'resources/skote/css/icons.min.css'
+],'public/css/app.css')
+    .scripts([
+        'resources/skote/js/jquery.min.js',
+        'resources/skote/js/bootstrap.bundle.min.js',
+        'resources/skote/js/metisMenu.min.js',
+        'resources/skote/js/simplebar.min.js',
+        'resources/skote/js/waves.min.js',
+        'resources/skote/js/apexcharts.min.js',
+        'resources/skote/js/dashboard.init.js',
+        'resources/skote/js/app.js'
+    ],'public/js/app.js');
